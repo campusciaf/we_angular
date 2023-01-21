4f7ef428d1e05d50ac41e0c9c852b74706e66c8e
campusciaf <sistemasdeinformacion@ciaf.edu.co>
2023-01-21 09:56:11 -0500

    inicio ajustes formulario

--- src/app/componente/inicio/inicio.component.ts
+++ src/app/componente/inicio/inicio.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { cliente } from 'src/app/clases/cliente';
 import { ConectarApiService } from 'src/app/servicios/conectar-api.service';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+
 
 declare var jQuery:any;
 declare var $:any;
@@ -28,7 +28,7 @@ export class InicioComponent implements OnInit {
     $("#dos").removeClass("active-link-dropdow");
   }
  
-  form!: FormGroup;
+ 
 
   micliente = new cliente('','','','');
   constructor(private conectarApiService:ConectarApiService,private formBuilder: FormBuilder) {
@@ -40,27 +40,74 @@ export class InicioComponent implements OnInit {
 
   ngOnInit() {
     this.activarLinkMenu();
+    this.buildForm();
 
   }
 
-
+  myForm!: FormGroup;
+ 
   private buildForm(){
-    this.form = this.formBuilder.group({
-      nombre: [0, Validators.min(18)],
-      correo: [0, Validators.min(18)],
-      celular: [0, Validators.min(18)],
-     
-    })
+    this.myForm = this.formBuilder.group({
+      nombre:['',{
+        validators: [
+          Validators.required, 
+          Validators.minLength(3),
+          Validators.maxLength(40)
+       ],
+       updateOn: 'blur'
+      }],
+      correo: ['', {
+        validators: [
+           Validators.required, 
+           Validators.email
+        ],
+        updateOn: 'blur'
+    }],
+    celular: ['', [Validators.required, Validators.minLength(11)]],
+    fo_programa: ['',[Validators.required,]],
+    acepto: ['',[Validators.required,Validators.requiredTrue]]
+    });
   }
 
+  get email() {
+    return this.myForm.controls['email'];
+}
+
+  get password() {
+      return this.myForm.controls['password'];
+  }
 
   agregar(){
-    console.log(this.form.value);
-      // this.conectarApiService.insertarCliente(this.micliente).subscribe(respuesta=>{
+    console.log(this.myForm.value);
+      // this.conectarApiService.insertarCliente(this.myForm.value).subscribe(respuesta=>{
       //   this.insertarNuevoCliente=respuesta;
       // }); 
 
   }
-
+  account_validation_messages = {
+    'nombre': [
+      { type: 'required', message: 'Campo de nombre requerido' },
+      { type: 'minlength', message: 'Su nombre debe tener al menos 6 caracteres' },
+      { type: 'maxlength', message: 'Su nombre no puede tener más de 33 caracteres' },
+      { type: 'pattern', message: 'Su nombre debe contener solo letras' },
+      { type: 'validUsername', message: 'Your username has already been taken' }
+    ],
+    'correo': [
+      { type: 'required', message: 'campo de correo requerido' },
+      { type: 'pattern', message: 'Enter a valid email' }
+    ],
+    'confirm_password': [
+      { type: 'required', message: 'Confirm password is required' },
+      { type: 'areEqual', message: 'Password mismatch' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required' },
+      { type: 'minlength', message: 'Password must be at least 5 characters long' },
+      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number' }
+    ],
+    'terms': [
+      { type: 'pattern', message: 'You must accept terms and conditions' }
+    ]
+    }
 
 }
