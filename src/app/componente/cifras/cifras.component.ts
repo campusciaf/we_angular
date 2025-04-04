4dc01c4615b91dde859a04a625fbdee212de46dd
Julian <sistemasdeinformacion@ciaf.edu.co>
2025-04-04 07:50:55 -0500

    ajustes

--- src/app/componente/cifras/cifras.component.ts
+++ src/app/componente/cifras/cifras.component.ts
@@ -27,8 +27,8 @@ export class CifrasComponent implements OnInit, AfterViewInit {
   ngOnInit() {
 
     this.counters = [
-      { target: 5000, value: 0 },
-      { target: 1000, value: 0 },
+      { target: 15, value: 0 },
+      { target: 6, value: 0 },
       { target: 0, value: 0 },
       { target: 0, value: 0 } // Inicializamos con 0
     ];
