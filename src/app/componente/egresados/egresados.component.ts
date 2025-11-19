ee42bdfc1b09af3d943a02af21e571eed6878ab9
senior-ciaf <senior.digital@ciaf.edu.co>
2025-11-19 14:04:44 -0500

    feat: Cambios en egresados

--- src/app/componente/egresados/egresados.component.ts
+++ src/app/componente/egresados/egresados.component.ts
@@ -32,40 +32,39 @@ export class EgresadosComponent {
     | undefined;
 
   slideConfig = {
-    slidesToShow: 6,
+    slidesToShow: 3,
     slidesToScroll: 1,
-    infinite: true,
+    dots: true,
+    arrows: true,
     autoplay: true,
+    autoplaySpeed: 3000,
     responsive: [
       {
-        breakpoint: 1048,
+        breakpoint: 1024,
         settings: {
-          slidesToShow: 4,
-        },
+          slidesToShow: 2
+        }
       },
       {
-        breakpoint: 778,
+        breakpoint: 600,
         settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-    ],
+          slidesToShow: 1
+        }
+      }
+    ]
   };
 
   public logo_pc = 'assets/image/egresados_pc.webp';
   public logo_m = 'assets/image/egresados_m.webp';
-  //public club_egresados="assets/image/club-egresados.webp";
+
   public seguimiento_egresados = 'assets/image/seguimiento_egresados.webp';
 
   public calendario="assets/image/calendario-regular.webp";
   public vistas="assets/image/vistas.webp";
 
+  public modelo_bienestar_egresados = 'assets/image/modelo_bienestar_egresados.webp';
+
+  
   public testimonio_egresado_1 = 'assets/image/testimonio-egresado-1.webp';
   public testimonio_egresado_2 = 'assets/image/testimonio-egresado-2.webp';
   public testimonio_egresado_3 = 'assets/image/testimonio-egresado-3.webp';
